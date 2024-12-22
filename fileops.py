15734bcc93e51e84e31f880b0b71086a695180f6
Ian Culleton <ianculleton@Ians-MacBook-Pro.local>
2024-12-22 11:28:55 -0500

    updates main function of FileMv

--- fileops.py
+++ fileops.py
@@ -1,20 +1,23 @@
 class FileMv:
   
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "origin": ("STRING", {"forceInput": True, "default": "", "dynamicPrompts": False}),
-                # Dropdown for regex or extension
-                "destination": ("STRING", {"default": "extension"}),
-            }
-        }
+  @classmethod
+  def INPUT_TYPES(cls):
+      return {
+          "required": {
+              "origin": ("STRING", {"forceInput": True, "default": "", "dynamicPrompts": False}),
+              # Dropdown for regex or extension
+              "destination": ("STRING", {"default": "extension"}),
+          }
+      }
 
-    # Outputs the index and filename
-    RETURN_TYPES = ("STRING")
-    RETURN_NAMES = ("Terminal Output")
+  # Outputs the index and filename
+  RETURN_TYPES = ("STRING")
+  RETURN_NAMES = ("Terminal Output")
 
-    OUTPUT_NODE = True
+  OUTPUT_NODE = True
 
-    FUNCTION = "mv_file"
-    CATEGORY = "IC/fileops"
+  FUNCTION = "main"
+  CATEGORY = "IC/fileops"
+  
+  def main(self, origin="", destination=""):
+    return (destination,)
